c8730f187f950786e17244363869273fa5b24a3d
Jacqueline Kalmár <jaqika@gmail.com>
2024-04-19 21:11:33 +0000

    Remove unregister_if_registered function
    remove the function unregister_if_registered and the calls to unregister the models before re-registering

--- scroll_core/admin.py
+++ scroll_core/admin.py
@@ -17,21 +17,7 @@ class GenreAdmin(admin.ModelAdmin):
     list_display = ('name',)
 
 
-# Define a function to unregister models safely
-def unregister_if_registered(model):
-    try:
-        admin.site.unregister(model)
-    except admin.sites.NotRegistered:
-        pass
-
-
-# Unregister previously registered models without customization
-unregister_if_registered(Book)
-unregister_if_registered(Author)
-unregister_if_registered(Genre)
-unregister_if_registered(BookAuthor)
-
-# Re-register models with custom admin configurations
+# Register models with custom admin configurations
 admin.site.register(Genre, GenreAdmin)
 admin.site.register(Author, AuthorAdmin)
 admin.site.register(Book, BookAdmin)
