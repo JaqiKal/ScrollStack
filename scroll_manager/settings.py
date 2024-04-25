3b06dedc442c15f1bd144bb206cee56f5be04cec
Jacqueline Kalmár <jaqika@gmail.com>
2024-04-25 16:29:13 +0000

    Fix: 500 server error when DEBUG=False
    Ttutor support assisted, commented out '#STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'

--- scroll_manager/settings.py
+++ scroll_manager/settings.py
@@ -227,7 +227,7 @@ STATICFILES_DIRS = [
 STATIC_ROOT = os.path.join(
     BASE_DIR, 'staticfiles')
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+#STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 
 # Cloudinary Setup
