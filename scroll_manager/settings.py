4a5c91cbaaca4ff96985ece4c3ae6d982a19389e
Jacqueline Kalmár <jaqika@gmail.com>
2024-04-30 17:21:43 +0000

    and

--- scroll_manager/settings.py
+++ scroll_manager/settings.py
@@ -127,8 +127,8 @@ TEMPLATES = [
             os.path.join(BASE_DIR, 'templates'),
             os.path.join(BASE_DIR, 'templates', 'allauth')
         ],
-        'APP_DIRS': False,
-        
+        'APP_DIRS': True,
+
         'OPTIONS': {
             'context_processors': [
                 'django.template.context_processors.debug',
