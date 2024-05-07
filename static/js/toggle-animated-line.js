d0e9e967b2eb67714238d563021d28a9c99581f2
Jacqueline Kalmár <jaqika@gmail.com>
2024-05-07 14:51:58 +0000

    Fix animation to always be on when logging in
    Refactor js to better handle on off
    
    Move script to base.html
    
    Refactor css animation
    
    In header.html add check if user is logged in or not

--- static/js/toggle-animated-line.js
+++ static/js/toggle-animated-line.js
@@ -1,22 +1,34 @@
 /**
  * This script manages the functionality of toggling the animated line in book-list.html
- * 
+ *
  * Other:
  * - jshint esversion: 6, please see README.md for further details
- * 
+ *
  * Author: JaqiKal
  * Date: May 2024
  */
 
 // jshint esversion: 6
 
+document.addEventListener("DOMContentLoaded", function () {
+    const animationToggle = document.getElementById('animation-toggle');
+    const flameDivider = document.querySelector('.flame-divider');
 
-// JavaScript to toggle animation class
-document.getElementById('animation-toggle').addEventListener('change', function () {
-    var flameDivider = document.querySelector('.flame-divider');
-    if (this.checked) {
-        flameDivider.classList.add('animate');
-    } else {
-        flameDivider.classList.remove('animate');
+    if (animationToggle && flameDivider) {
+        // Toggle the class based on the initial state
+        if (animationToggle.checked) {
+            flameDivider.classList.add('animate');
+        } else {
+            flameDivider.classList.remove('animate');
+        }
+
+        // Add an event listener to toggle the class on change
+        animationToggle.addEventListener('change', function () {
+            if (this.checked) {
+                flameDivider.classList.add('animate');
+            } else {
+                flameDivider.classList.remove('animate');
+            }
+        });
     }
 });
